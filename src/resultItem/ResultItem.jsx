d41be712580b5ec7232c09589dd3518bc065aded
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-02-16 20:02:02 -0600

    adjusted image and text box styles for displayed results

--- src/resultItem/ResultItem.jsx
+++ src/resultItem/ResultItem.jsx
@@ -5,9 +5,9 @@ import "./resultItem.scss";
 const ResultItem = ({ item }) => {
   return (
     <article className="card">
-      <div className="card__image-box">
-        {/* <img src={item.strMealThumb} alt={item.strMeal} /> */}
-      </div>
+      {/* <div className="card__image-box">
+      </div> */}
+      <img src={item.strMealThumb} alt={item.strMeal} />
       <div className="card__text-box">
         <span className="card__title">{item.strMeal}</span>
         <button className="card__btn">View Receipe</button>
